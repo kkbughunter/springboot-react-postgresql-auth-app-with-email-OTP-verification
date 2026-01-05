e7feebf04e8b3fafea48bb9141d49da01bccf6c6
karthikeyan <karthikeyan@nadalgroups.onmicrosoft.com>
2026-01-05 15:05:33 +0530

    frontend init

---
+++ multi-user-auth-app/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <p className="text-xl font-semibold">
+        Hello World
+      </p>
+    </div>
+  )
+}
+
+export default App
